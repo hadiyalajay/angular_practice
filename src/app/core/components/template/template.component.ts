77d01644f8c1296dba63f34ec336a0b14630271b
hadiyalajay <mailto:hadiyalajay112@gmail.com>
2023-10-12 18:07:09 +0530

    -  Second change

---
+++ src/app/core/components/template/template.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-template',
+  templateUrl: './template.component.html',
+  // template:`<p>Template</p>`,
+  styleUrls: ['./template.component.scss'],
+  // styles:[`
+  // p
+  // {
+  //   padding:10px;
+  //   background-color:skyblue;
+  //   border-radius:5px;
+  // }`
+  // ]
+})
+export class TemplateComponent {
+
+}
